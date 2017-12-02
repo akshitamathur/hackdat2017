2e2247029dc8e70dff68b548d096b22e87580ca4
Akshita <akshita.mathur@gmail.com>
2017-12-02 15:38:27 -0500

    New Idea - Akshitas First Commit

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 (function(){
 	console.log("It's working!");
+
 })();
 
-function options(){
-	var newDiv = document.getElementById("options");
-	var getStarted = document.getElementById("startButton");
-	 $('<div id="xmas" class="animated fadeIn"><img src="./images/xmas.png" alt="Xmas Tree" width="300" height="350"></div>').appendTo('#options');
-	 $('<div id="bday" class="animated fadeIn"><img src="./images/bday.png" alt="Balloons" width="250" height="300"></div>').appendTo('#options');
-	 getStarted.fadeOut();
-};
+// function options(){
+// 	var newDiv = document.getElementById("options");
+// 	var getStarted = document.getElementById("startButton");
+// 	 $('<div id="xmas" class="animated fadeIn"><img src="./images/xmas.png" alt="Xmas Tree" width="300" height="350"></div>').appendTo('#options');
+// 	 $('<div id="bday" class="animated fadeIn"><img src="./images/bday.png" alt="Balloons" width="250" height="300"></div>').appendTo('#options');
+// 	 getStarted.fadeOut();
+// };
