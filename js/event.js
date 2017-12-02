a395f3a4c35feb129d8cfd9ac78b6e81f659bff1
Akshita <akshita.mathur@gmail.com>
2017-12-02 17:46:11 -0500

    background and styling rehaul

--- js/event.js
+++ js/event.js
@@ -1,4 +1,31 @@
 (function(){
 	console.log("It's working.");
 
+
+
+
+
 })();
+
+// (function games()
+// {
+//     var ss = document.getElementById("ss").checked;;
+   
+//     if(ss == true){
+//     	console.log("Secret Santa Checked.");
+//     }else{
+//     	 return false;
+//     }
+
+
+// })();
+
+	function printChecked(){
+				var items=document.getElementsByName('acs');
+				var selectedItems="";
+				for(var i=0; i<items.length; i++){
+					if(items[i].type=='checkbox' && items[i].checked==true)
+						selectedItems+=items[i].value+"\n";
+				}
+			console.log("Print Checked Was Successful - function exit");
+			}
