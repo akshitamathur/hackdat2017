c8910c7e229a0fd1cf14981352d67656151b241c
Gracie <gracie.zhang.email@gmail.com>
2017-12-02 19:15:15 -0500

    Add map and new acount page

--- js/main.js
+++ js/main.js
@@ -2,3 +2,15 @@
 	console.log("It's working!");
 
 })();
+function initMap() 
+{
+  var toronto = {lat: 43.6532, lng: -79.3832};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: toronto
+  });
+  var marker = new google.maps.Marker({
+    position: toronto,
+    map: map
+  });
+}
